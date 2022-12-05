ba43c439a309d2716084416a9fc1869c9d2cf0ad
Nicolas Sierra <nico.sierra13@gmail.com>
2022-12-05 13:38:26 -0500

    Refactor About section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const AboutSection = ({ title, ProfileImage, LinkButton }) => {
+  return (
+    <>
+      <div className="about-image">
+        <img src={ProfileImage} />
+      </div>
+      <div className="about-content">
+        <h2 className="about-title text-center">{title}</h2>
+        <div className="content">
+          <div className="about-parragraph">
+            <p className="parragraph">
+              Creating digital-multimedia solutions is what I work for,
+              combining my design skills and engineering to develop high quality
+              products and experiences.With knowledge in front-end development
+              and API Rest consuming services using technologies like
+              Javascript, React.js, redux, Next.js, preprocessors as SASS and
+              css frameworks such as tailwind, handling software versioning with
+              git and implementing gitflow branching methodology.<br></br> As
+              well with solid knowledge on UX/UI design, quantitative /
+              qualitative research, and atomic design, to provide users the
+              better solution for their experiences on different platforms.Daily
+              learning to improve and innovate with my work is my main goal,
+              challenging myself with new projects, open to learn new
+              technologies to work with in multidisciplinary teams
+            </p>
+            <div className="separator"></div>
+          </div>
+          <div className="about-button">
+            <LinkButton text="Download CV" style="black" styleBg="black-bg" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutSection;
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from "../components/Hero";
 import Layout from "../layout/Layout";
 import ProfileImage from "../../assets/img/Profile.png";
 import LinkButton from "../components/buttons/LinkButton";
+import AboutSection from "../components/AboutSection";
+
 const Home = () => {
   return (
     <Layout>
@@ -17,42 +19,11 @@ const Home = () => {
             />
           </div>
           <div className="about about-section">
-            <div className="about-image">
-              <img src={ProfileImage} />
-            </div>
-            <div className="about-content">
-              <h2 className="about-title text-center">
-                About <br></br> Me
-              </h2>
-              <div className="content">
-                <div className="about-parragraph">
-                  <p className="parragraph">
-                    Creating digital-multimedia solutions is what I work for,
-                    combining my design skills and engineering to develop high
-                    quality products and experiences.With knowledge in front-end
-                    development and API Rest consuming services using
-                    technologies like Javascript, React.js, redux, Next.js,
-                    preprocessors as SASS and css frameworks such as tailwind,
-                    handling software versioning with git and implementing
-                    gitflow branching methodology. As well with solid knowledge
-                    on UX/UI design, quantitative / qualitative research, and
-                    atomic design, to provide users the better solution for
-                    their experiences on different platforms.Daily learning to
-                    improve and innovate with my work is my main goal,
-                    challenging myself with new projects, open to learn new
-                    technologies to work with in multidisciplinary teams
-                  </p>
-                  <div className="separator"></div>
-                </div>
-                <div className="about-button">
-                  <LinkButton
-                    text="Download CV"
-                    style="black"
-                    styleBg="black-bg"
-                  />
-                </div>
-              </div>
-            </div>
+            <AboutSection
+              title={"About me"}
+              ProfileImage={ProfileImage}
+              LinkButton={LinkButton}
+            />
           </div>
         </div>
       </main>
